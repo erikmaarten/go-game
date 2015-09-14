85b8ba00937dfc10fc7a05a093c8b59d83b59ac3
Erik Andersson <erik.andersson@attentec.se>
2015-09-14 12:26:38 +0200

    removed unneeded functions

--- client/GameInfo.jsx
+++ client/GameInfo.jsx
@@ -3,31 +3,12 @@ GameInfo = React.createClass({
     players: React.PropTypes.array.isRequired
   },
 
-  playerOfColor(color) {
-    return _.chain(this.props.players)
-      .filter(function(player) {
-        player.color === color;
-      })
-      .value()[0];
-  },
-
-  test() {
-    console.log("test");
-    /*
-        <p>Black: {this.playerOfColor("black").userId}</p>
-        <p>White: {this.playerOfColor("white").userId}</p>
-        */
-  },
-
-  pColor(players, color) {
-
-  },
-
   render() {
     return (
       <div>
       <p>{this.test()}</p>
-      <p>Black: {this.playerOfColor("black")}</p>
+      <p>Black: {this.props.players[0].userId}</p>
+      <p>White: {this.props.players[1].userId}</p>
       {/*<p>{this.playerOfColor()}</p>*/}
 
       </div>
