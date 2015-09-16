4362498f2def82decd4816c7841d98a52d3b75fa
Erik Andersson <erik.andersson@attentec.se>
2015-09-16 15:42:29 +0200

    background grid implemented as canvas

---
+++ client/grid_canvas/grid_canvas.js
@@ -0,0 +1,64 @@
+GridCanvas = {};
+
+GridCanvas.positionAndRender = function() {
+  var firstBoardElement = $('.intersection')[0].getBoundingClientRect();
+  var top = firstBoardElement.top;
+  var width = firstBoardElement.width;
+  var left = firstBoardElement.left;
+  var canvas = document.getElementById("grid-canvas");
+  var board = Session.get("board");
+  var unitsWide = Game.getBoardWidth(board);
+
+  canvas.style.top = top + "px";
+  canvas.style.left = left + "px";
+  GridCanvas.renderGrid("grid-canvas", unitsWide, unitsWide*width, width);
+}
+
+GridCanvas.renderGrid = function(canvasId, boardWidthUnits, boardWidthPx, unitWidthPx) {
+  // also set up window resize event listener here?
+  check(canvasId, String);
+  check(boardWidthUnits, Number);
+  check(boardWidthPx, Number);
+  check(unitWidthPx, Number);
+
+  var canvas = document.getElementById(canvasId);
+  var context = canvas.getContext('2d');
+  this.setCanvasSize(canvas, boardWidthPx);
+  this.draw(context, boardWidthUnits, boardWidthPx, unitWidthPx);
+}
+
+GridCanvas.setCanvasSize = function(canvasEl, boardWidthInPixels) {
+  canvasEl.width = boardWidthInPixels;
+  canvasEl.height = boardWidthInPixels;
+}
+
+GridCanvas.draw = function(canvasContext, boardWidthUnits, boardWidthPx, unitWidthPx) {
+  // Begin drawing lines from middle of board squares, since intersections on the board
+  // will be represented by lines crossing each other
+
+  var halfUnitPx = unitWidthPx / 2;
+
+  // draw vertical lines
+  for (var x = 0; x < boardWidthUnits; ++x) {
+    var start_x = x*unitWidthPx + halfUnitPx;
+    var start_y = 0 + halfUnitPx;
+    var end_x = start_x;
+    var end_y = start_y + (boardWidthUnits - 1) * unitWidthPx;
+    canvasContext.moveTo(start_x, start_y);
+    canvasContext.lineTo(end_x, end_y);
+  }
+
+  // draw horizontal lines
+  for (var y = 0; y < boardWidthUnits; ++y) {
+    var start_x = 0 + halfUnitPx;
+    var start_y = y*unitWidthPx + halfUnitPx;
+    var end_x = start_x + (boardWidthUnits - 1) * unitWidthPx;
+    var end_y = start_y;
+    canvasContext.moveTo(start_x, start_y);
+    canvasContext.lineTo(end_x, end_y);
+  }
+
+  canvasContext.strokeStyle = "#eee";
+
+  canvasContext.stroke();
+}
--- go-game.jsx
+++ go-game.jsx
@@ -3,6 +3,19 @@ if (Meteor.isClient) {
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
+
+    Meteor.setTimeout(function() {
+      GridCanvas.positionAndRender();
+    }, 1000);
+
+    var resizeTimer;
+    $(window).bind('resize', function() {
+      Meteor.clearTimeout(resizeTimer);
+      resizeTimer = Meteor.setTimeout(function() {
+        GridCanvas.positionAndRender();
+      }, 50);
+    });
+
   });
 }
 
