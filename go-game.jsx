862712211ced1cd60aa82a834183af2f5a058335
Erik Andersson <erik.andersson@attentec.se>
2015-09-22 13:06:34 +0200

    moved login button and adjusted container layouts

--- go-game.jsx
+++ go-game.jsx
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
   // This code is executed on the client only
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
-    React.render(<LeftSidebar />, document.getElementById("left-sidebar"));
+    React.render(<LeftSidebar />, document.getElementById("left-sidebar-render-target"));
     React.render(<App />, document.getElementById("render-target"));
 
     $(window).bind('resize', function() {
